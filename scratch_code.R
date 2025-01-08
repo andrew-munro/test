083b22bcb03af77593ab9b03e718473419c25517
armunro <andrew.munro@alaska.gov>
2025-01-08 13:50:27 -0900

    Grrr #2

--- scratch_code.R
+++ scratch_code.R
@@ -1,17 +1,6 @@
 
 library("readxl")
 library("tidyverse")
-<<<<<<< HEAD
-library(reshape2)
-
-# Read data file provided for ESR that includes commercial harvest by management area
-ADFG <- read_csv("data/Salmon Landings for ESR_Sept 2023.csv")
-
-#clean up and simplify
-ADFG2 <- filter(ADFG, between (`DFB Year`,1985,2015))
-colnames(ADFG2) <- c("Year", "Manage_Area", "Region_No","Region","Species_Code", "Species","Landed_Wt", "Whole_Wt","Number","Processor", "Permits","Vessels")
-
-=======
 library("reshape2")
 
 # Read data file provided for ESR that includes commercial harvest by management area
@@ -20,7 +9,6 @@ ADFG <- read_csv("data/Salmon Landings for ESR_Sept 2023.csv",show_col_types = F
 #clean up and simplify
 colnames(ADFG) <- c("Year", "Manage_Area", "Region_No","Region","Species_Code", "Species","Landed_Wt", "Whole_Wt","Number","Processor", "Permits","Vessels")
 ADFG2 <- filter(ADFG, between (Year,1985,2015))
->>>>>>> 178f16d8f6b3ded8559093d94cabb8c9c7ecd553
 
 ADFG2 <- ADFG2 %>% 
   mutate(Species = ifelse(as.character(Species) == "salmon, chinook", "chinook", as.character(Species)),
@@ -36,34 +24,34 @@ ADFG2 <- ADFG2 %>%
          Region = ifelse(as.character(Region) == "Westward Region", "Westward", as.character(Region))
          )
 
-<<<<<<< HEAD
-ADFG <- cbind(ADFG,ADFG[,8]*0.45359237)
-colnames(ADFG)[13] <- "Wt_kg"       
-=======
 ADFG3 <- filter(ADFG2, Manage_Area != "9" & Species %in% c("pink","chum","sockeye"))
 
 ADFG4 <- ADFG3 %>%
         group_by(Region, Species, Year) %>% summarise(Number=sum(Number) )
 
-ADFG <- cbind(ADFG,ADFG[,8]*0.45359237)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# scratch code to calculate average weight.  The metric tons conversion is what was used by S. Donellan in OceanAK
+ADFG2 <- data.frame(ADFG2,ADFG2[,8]/2204.62262) # convert to metric tons
+ADFG <- cbind(ADFG,ADFG[,8]*0.45359237) # convert to kg
 colnames(ADFG)[13] <- "Wt_kg"       
 
->>>>>>> 178f16d8f6b3ded8559093d94cabb8c9c7ecd553
 ADFG <- cbind(ADFG,ADFG$Wt_kg/ADFG$`Number Of Fish (estimated)`)
 colnames(ADFG)[14] <- "avg_wt" 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 ADFG_pink <- filter(ADFG, ADFG$`Species Name` == "salmon, pink" & between (`DFB Year`,1985,2015))
 
 w <- ifelse((ADFG$`Management Area` == "A") | (ADFG$`Management Area` == "B") | (ADFG$`Management Area` == "C") | (ADFG$`Management Area` == "D"), "SEAK", +
               ifelse((ADFG$`Management Area` == "E"), "PWS", ifelse((ADFG$`Management Area` == "H"), "CI", ifelse((ADFG$`Management Area` == "K") | (ADFG$`Management Area` == "L"), "KOD","Other"))  ))   
 
-<<<<<<< HEAD
 p <- ggplot(data = ADFG_pink, aes(x = `DFB Year`, y = `Number Of Fish (estimated)`)) + geom_point()
 p + facet_wrap(~`Management Area`)  
-=======
+
 p <- ggplot(data = ADFG4, aes(x = Year, y = Number)) + geom_point()
 p + facet_wrap(~Region+Species, scales = "free")  
->>>>>>> 178f16d8f6b3ded8559093d94cabb8c9c7ecd553
+
 
 
 
